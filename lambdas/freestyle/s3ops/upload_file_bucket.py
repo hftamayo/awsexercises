a423a895cfe4397015001b7bd2a09ee2049ff489
Herbert Tamayo UES <herbert.fernandez@ues.edu.sv>
2024-08-20 16:10:14 +0000

    imrpving upload file confirm message

--- lambdas/freestyle/s3ops/upload_file_bucket.py
+++ lambdas/freestyle/s3ops/upload_file_bucket.py
@@ -5,4 +5,4 @@ client = boto3.client('s3')
 #upload a file to s3 bucket
 client.upload_file('dailysales.txt', 's3automatebucket', 'dailysales.txt')
 
-print("File uploaded successfully")
+print("the sales file uploaded successfully")
